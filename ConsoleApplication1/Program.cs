7fc8d62dcc94f74974309e345909a493b2f65e0f
Venkatesh Mahadev <venkatesh.mahadev@gmail.com>
2010-05-23 11:24:41 -0700

    initial commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Shapes;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AutoResetEvent a = new AutoResetEvent(false);
+            a.Set();
+            a.WaitOne();
+        }
+    }
+}
